ece799823a31a70512c44a7bed4e69041c097ffa
Digbijaya Lenka <digbijayalenka@gmail.com>
2024-11-06 02:43:50 +0530

    code pushed for MacBook

--- frontend/notes-app/src/components/Cards/NoteCard.jsx
+++ frontend/notes-app/src/components/Cards/NoteCard.jsx
@@ -1,7 +1,25 @@
-import React from 'react'
+import React from "react";
 
-export default function NoteCard() {
+export default function NoteCard({
+  title,
+  contnet,
+  tags,
+  isPinned,
+  onEdit,
+  onDelete,
+  onPinNote,
+}) {
   return (
-    <div>NoteCard</div>
-  )
+    <div>
+      <div className="">
+        <div>
+          <h6 className="text-sm font-medium">{title}</h6>
+          <span className="text-xs text-slate-500">{date}</span>
+        </div>
+
+        <MdOutlinePushPin className="" onClick={onPinNote} />
+      </div>
+      <p className="">{contnet?.slice(0, 60)}</p>
+    </div>
+  );
 }
