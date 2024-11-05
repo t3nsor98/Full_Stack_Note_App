90e0b53161281e19d5ff7a95b704ac3972a79206
Digbijaya Lenka <digbijayalenka@gmail.com>
2024-11-06 04:05:55 +0530

    added notecard component

--- frontend/notes-app/src/components/Cards/NoteCard.jsx
+++ frontend/notes-app/src/components/Cards/NoteCard.jsx
@@ -1,25 +1,44 @@
 import React from "react";
-
+import { MdOutlinePushPin } from "react-icons/md";
+import { MdCreate, MdDelete } from "react-icons/md";
 export default function NoteCard({
   title,
-  contnet,
+  content,
   tags,
   isPinned,
   onEdit,
   onDelete,
   onPinNote,
+  date,
 }) {
   return (
-    <div>
-      <div className="">
+    <div className="border rounded p-4 bg-white hover:shadow-xl transition-all ease-in-out">
+      <div className="flex items-center justify-between">
         <div>
           <h6 className="text-sm font-medium">{title}</h6>
           <span className="text-xs text-slate-500">{date}</span>
         </div>
 
-        <MdOutlinePushPin className="" onClick={onPinNote} />
+        <MdOutlinePushPin
+          className={`icon-btn ${isPinned ? "text-primary" : "text-slate-400"}`}
+          onClick={onPinNote}
+        />
       </div>
-      <p className="">{contnet?.slice(0, 60)}</p>
+      <p className="text-xs text-slate-600 mt-2">{content?.slice(0, 60)}</p>
+
+      <div className="flex items-center justify-between mt-2">
+        <div className="text-xs text-slate-500">{tags}</div>
+        <div className="flex items-center gap-2">
+          <MdCreate
+            className="icon-btn hover:text-green-600 "
+            onClick={onEdit}
+          />
+          <MdDelete
+            className="icon-btn hover:text-red-600 "
+            onClick={onDelete}
+          />
+        </div>
+      </div>
     </div>
   );
 }
--- frontend/notes-app/src/pages/Home/Home.jsx
+++ frontend/notes-app/src/pages/Home/Home.jsx
@@ -1,14 +1,33 @@
 // import React from 'react'
 import Navbar from "../../components/Navbar/Navbar";
 import NoteCard from "../../components/Cards/NoteCard";
+import { MdAdd } from "react-icons/md";
 export default function Home() {
   return (
     <div>
       <>
         <Navbar />
         <div className="container mx-auto">
-          <NoteCard />
+          <div className="grid grid-cols-3 gap-4 mt-8">
+            <NoteCard
+              title="meeting on 31st december"
+              date="31-12-2024"
+              content="meeting on 31st december"
+              tags="#meeting"
+              isPinned={true}
+              onEdit={() => {}}
+              onDelete={() => {}}
+              onPinNote={() => {}}
+            />
+          </div>
         </div>
+
+        <button
+          className="w-16 h-16 flex items-center justify-center rounded-2xl bg-primary hover:bg-blue-600 absolute right-10 bottom-10"
+          onClick={() => {}}
+        >
+          <MdAdd className="text-[32px] text-white" />
+        </button>
       </>
     </div>
   );
