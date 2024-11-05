db02e8eadb1d60a85bbed7545920d2a7d7199e48
Digbijaya Lenka <digbijayalenka@gmail.com>
2024-11-06 02:21:14 +0530

    Done till NoteCard component

--- frontend/notes-app/src/components/Navbar/Navbar.jsx
+++ frontend/notes-app/src/components/Navbar/Navbar.jsx
@@ -1,22 +1,33 @@
-import React from "react";
+import { useState } from "react";
 import ProfileInfo from "../Cards/ProfileInfo";
 import { useNavigate } from "react-router-dom";
 import SearchBar from "../SearchBar/SearchBar";
 
 export default function Navbar() {
+  const [searchQuery, setSearchQuery] = useState("");
+
+  const handleSearch = () => {};
+
+  const onClearSearch = () => {
+    setSearchQuery("");
+  };
+
   const navigate = useNavigate();
   const onLogout = () => {
     navigate("/login");
-    
-  }
+  };
   return (
     <div className="bg-white flex items-center justify-between px-6 py-2 drop-shadow">
       <h2 className="text-xl font-medium text-black py-2">Notes App</h2>
-      
-      <SearchBar />
+
+      <SearchBar
+        value={searchQuery}
+        onChange={({target}) => setSearchQuery(target.value)}
+        handleSearch={handleSearch}
+        onClearSearch={onClearSearch}
+      />
 
       <ProfileInfo onLogout={onLogout} />
-
     </div>
   );
 }
--- frontend/notes-app/src/pages/Home/Home.jsx
+++ frontend/notes-app/src/pages/Home/Home.jsx
@@ -1,10 +1,14 @@
 // import React from 'react'
 import Navbar from "../../components/Navbar/Navbar";
+import NoteCard from "../../components/Cards/NoteCard";
 export default function Home() {
   return (
     <div>
       <>
-      <Navbar />
+        <Navbar />
+        <div className="container mx-auto">
+          <NoteCard />
+        </div>
       </>
     </div>
   );
