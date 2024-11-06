d2ec92bab8e3baca842c071aa48a0f8f2ff67518
Digbijaya Lenka <digbijayalenka@gmail.com>
2024-11-07 03:03:31 +0530

    Front-end part is done

--- frontend/notes-app/src/pages/Home/AddEditNotes.jsx
+++ frontend/notes-app/src/pages/Home/AddEditNotes.jsx
@@ -1,14 +1,53 @@
-import React from "react";
+import React, { useState } from "react";
+import TagInput from "../../components/Input/TagInput";
+import { MdClose } from "react-icons/md";
 
-export default function AddEditNotes() {
+export default function AddEditNotes({ onClose, type, noteData }) {
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
+  const [tags, setTags] = useState([]);
+
+  const [error, setError] = useState(null);
+
+  // add note
+  const addNewNote = async () => {};
+  // edit note
+  const editNote = async () => {};
+
+  const handleAddNote = () => {
+    if (!title) {
+      setError("Title is required");
+      return;
+    }
+    if (!content) {
+      setError("Content is required");
+      return;
+    }
+    setError(null);
+
+    if (type === "edit") {
+      editNote();
+    } else {
+      addNewNote();
+    }
+  };
   return (
-    <div>
+    <div className="relative">
+      <button
+        className="w-10 h-10 rounded-full flex items-center justify-center absolute -top-3 -right-3 hover:bg-slate-500"
+        onClick={onClose}
+      >
+        <MdClose className="text-xl text-slate-400" />
+      </button>
+
       <div className="flex flex-col gap-2">
         <label className="input-label">TITLE</label>
         <input
           type="text"
           className="text-2xl text-slate-950 outline-none"
           placeholder="Go to gym at 5"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
         />
       </div>
 
@@ -19,6 +58,8 @@ export default function AddEditNotes() {
           className="text-sm text-slate-950 outline-none bg-slate-50 p-2 rounded"
           placeholder="Content"
           rows={10}
+          value={content}
+          onChange={(e) => setContent(e.target.value)}
         />
       </div>
 
@@ -26,9 +67,14 @@ export default function AddEditNotes() {
         <label htmlFor="" className="input-label">
           TAGS
         </label>
+        <TagInput tags={tags} setTags={setTags} />
       </div>
 
-      <button className="btn-primary font-medium mt-5 p-3" onClick={() => {}}>
+      {error && <p className="text-sm text-red-500 pt-4">{error}</p>}
+      <button
+        className="btn-primary font-medium mt-5 p-3"
+        onClick={handleAddNote}
+      >
         ADD
       </button>
     </div>
--- frontend/notes-app/src/pages/Home/Home.jsx
+++ frontend/notes-app/src/pages/Home/Home.jsx
@@ -1,9 +1,17 @@
-// import React from 'react'
+import React, { useState } from "react";
 import Navbar from "../../components/Navbar/Navbar";
 import NoteCard from "../../components/Cards/NoteCard";
 import { MdAdd } from "react-icons/md";
 import AddEditNotes from "./AddEditNotes";
+import Modal from "react-modal";
+
 export default function Home() {
+  const [openAddEditModal, setOpenAddEditModal] = useState({
+    isShown: false,
+    type: "add",
+    data: null,
+  });
+
   return (
     <div>
       <>
@@ -25,12 +33,36 @@ export default function Home() {
 
         <button
           className="w-16 h-16 flex items-center justify-center rounded-2xl bg-primary hover:bg-blue-600 absolute right-10 bottom-10"
-          onClick={() => {}}
+          onClick={() => {
+            setOpenAddEditModal({
+              isShown: true,
+              type: "add",
+              data: null,
+            });
+          }}
         >
           <MdAdd className="text-[32px] text-white" />
         </button>
 
-        <AddEditNotes />
+        <Modal
+          isOpen={openAddEditModal.isShown}
+          onRequestClose={() => {}}
+          style={{
+            overlay: {
+              backgroundColor: "rgba(0,0,0,0.2)",
+            },
+          }}
+          contentLabel=""
+          className="w-[40%] max-h-3/4 bg-white rounded-md mx-auto mt-14 p-5 overflow-scroll "
+        >
+          <AddEditNotes
+            type={openAddEditModal.type}
+            noteData={openAddEditModal.data}
+            onClose={() =>
+              setOpenAddEditModal({ ...openAddEditModal, isShown: false })
+            }
+          />
+        </Modal>
       </>
     </div>
   );
