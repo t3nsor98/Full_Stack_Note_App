f0ad120309c70adbe9863c78f4c6e270b75ceabf
Digbijaya Lenka <digbijayalenka@gmail.com>
2024-11-10 03:55:38 +0530

    added login API in backend

--- backend/index.js
+++ backend/index.js
@@ -58,6 +58,48 @@ app.post("/create-account", async (req, res) => {
   });
 });
 
+// login
+app.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+  if (!email || !password) {
+    return res
+      .status(400)
+      .json({ error: true, message: "All fields are required" });
+  }
+  const userInfo = await User.findOne({ email: email });
+
+  if (!userInfo) {
+    return res.json({ error: true, message: "User doesn't exist" });
+  }
+
+  if (userInfo.password !== password) {
+    return res.json({ error: true, message: "Password doesn't match" });
+  }
+
+  if (userInfo.email == email && userInfo.password == password) {
+    const accessToken = jwt.sign(
+      { userInfo },
+      process.env.ACCESS_TOKEN_SECRET,
+      {
+        expiresIn: "1d",
+      }
+    );
+
+    return res.json({
+      error: false,
+      accessToken,
+      user: userInfo,
+      message: "Login Successfully",
+    });
+  } else {
+    return res
+      .status(400)
+      .json({ error: true, message: "Password doesn't match" });
+  }
+});
+
+//Add Note
+
 app.listen(8000);
 
 module.exports = app;
