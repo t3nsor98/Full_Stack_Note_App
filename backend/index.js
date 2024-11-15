a6bc11f5ee49414dab5f49a6fa14f8ac5550e303
Digbijaya Lenka <digbijayalenka@gmail.com>
2024-11-15 12:03:27 +0530

    update the port for render

--- backend/index.js
+++ backend/index.js
@@ -264,7 +264,9 @@ app.get("/search-notes", authenticateToken, async (req, res) => {
   }
 });
 
-app.listen(8000);
+app.listen(process.env.PORT || 8000, () => {
+  console.log(`Server is running on port ${process.env.PORT}`);
+});
 
 module.exports = app;
 
